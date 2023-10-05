9c35b5efed555366d9618f6e4f9ed7318b3818e4
Nokestro <Nokestro@yandex.by>
2023-10-05 20:20:26 +0300

    auth

--- UserLogin.py
+++ UserLogin.py
@@ -1,4 +1,7 @@
-class UserLogin():
+from flask import url_for
+from flask_login import UserMixin
+
+class UserLogin(UserMixin):
     def fromDB(self, user_id, db):
         self.__user = db.getUser(user_id)
         return self
@@ -6,15 +9,40 @@ class UserLogin():
     def create(self, user):
         self.__user = user
         return self
-
-    def is_authenticated(self):
-        return True
-
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
+    # Есть в Mixin
+    # def is_authenticated(self):
+    #     return True
+    #
+    # def is_active(self):
+    #     return True
+    #
+    # def is_anonymous(self):
+    #     return False
 
     def get_id(self):
         return str(self.__user['id'])
+
+    def getName(self):
+        return str(self.__user['name']) if self.__user['name'] else "Без имени"
+
+    def getEmail(self):
+        return str(self.__user['email']) if self.__user['email'] else "Без email"
+
+    def getAvatar(self, app):
+        img = None
+        if not self.__user['avatar']:
+            try:
+                with app.open_resource(app.root_path + url_for('static', filename='image/default.png'), "rb") as f:
+                    img = f.read()
+
+            except FileNotFoundError as e:
+                print("Не найден аватар по умолчанию")
+            else:
+                img = self.__user['avatar']
+            return img
+
+    def verifyExt(self, filename):
+        ext = filename.rsplit('.', 1)[1]
+        if ext == "png" or ext == "PNG":
+            return True
+        return False
--- kitty.py
+++ kitty.py
@@ -4,15 +4,16 @@ import os
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from flask_login import LoginManager,  login_user, login_required
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from UserLogin import UserLogin
 from Fdatabase import FDataBase
+from forms import LoginForm
 
 # Конфигурация
 DATABASE =  '/tmp/flsite.db'
 SECRET_KEY = 'fasko234k2podsa3r233ew'
 DEBUG = True
-
+MAX_CONTENT_LENGTH = 1024 * 1024
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -20,6 +21,9 @@ app.config.from_object(__name__)
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = "Авторизуйтесь пожалуйста для доступа к контенту"
+login_manager.login_message_category = "sussecc"
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -101,15 +105,32 @@ def login():
 
 @app.route('/log', methods=["POST", "GET"])
 def log():
-    if request.method = "POST":
-        user = dbase.getUserByEmail(request.form['email'])
-        if user and check_password_hash(user['psw'], request.form['psw']):
+    if current_user.is_authenticated:
+        return redirect(url_for('uprofile'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+    user = dbase.getUserByEmail(form.email.data)
+        if user and check_password_hash(user['psw'], form.psw.data):
             userlogin = UserLogin().create(user)
-            login_user(userlogin)
-            return redirect('kitty/index.html')
+            rm = form.remember.data
+            login_user(userlogin, remember=rm)
+            return redirect(request.args.get('next') or url_for('uprofile'))
+
         flash('Невераня пара пароль/логин', 'error')
 
-    return render_template('kitty/log.html', title='Логин', menu=dbase.getMenu())
+    return render_template('kitty/log.html', menu=dbase.getMenu(), title='Авторизация', form=form)
+
+    # if request.method == "POST":
+    #     user = dbase.getUserByEmail(request.form['email'])
+    #     if user and check_password_hash(user['psw'], request.form['psw']):
+    #         userlogin = UserLogin().create(user)
+    #         rm = True if request.form.get('remainme') else False
+    #         login_user(userlogin, remember=rm)
+    #         return redirect(request.args.get('next') or url_for('uprofile'))
+    #     flash('Невераня пара пароль/логин', 'error')
+
+    # return render_template('kitty/log.html', title='Логин', menu=dbase.getMenu())
 @app.route('/register', methods=["POST", "GET"])
 def register():
     if request.method == "POST":
@@ -176,6 +197,48 @@ def visit():
     return f"<p>Число просмотров = {session['visits']}</p>"
 
 
+@app.route('/uprofile')
+@login_required
+def uprofile():
+    return render_template('kitty/uprofile.html', menu = dbase.getMenu(), title="profile")
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Вы вышли из аккаунта", "success")
+    return redirect(url_for('login'))
+
+
+
+@app.route('/userava')
+@login_required
+def userava():
+    img = current_user.getAvatar(app)
+    if not img: return ""
+    h=make_response(img)
+    h.headers['Content-Type'] = 'image/png'
+
+
+@app.route('/upload', methods=["POST", "GET"])
+@login_required
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and current_user.verifyExt(file.filename):
+            try:
+                img = file.read()
+                res = dbase.updateUserAvatar(img, current_user.get_id())
+                if not res:
+                    flash("Ошибка обновления аватара", "error")
+                    return redirect(url_for('profile'))
+                flash("Аватар обновлен", "success")
+            except FileNotFoundError as e:
+                flash("Ошибка чтения файла", "error")
+        else:
+            flash("Ошибка обновления аватара", "error")
+
+    return redirect(url_for('profile'))
 # Обработчик ошибок
 
 @app.errorhandler(404)
